336914ae245b2269493e8af94530dca6a5d39ad6
João Victor G. de Carvalho <136175136+iaevitin7@users.noreply.github.com>
2023-06-24 15:13:58 -0300

    Upload WebBrowser - Project

---
+++ 11-WebBrowser/WebBrowser/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace WebBrowser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txbLink = new System.Windows.Forms.TextBox();
+            this.btnLink = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel2 = new System.Windows.Forms.LinkLabel();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(79, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(479, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Vídeo do Youtube com WebBrowser";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Link:";
+            // 
+            // txbLink
+            // 
+            this.txbLink.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbLink.Location = new System.Drawing.Point(46, 53);
+            this.txbLink.Name = "txbLink";
+            this.txbLink.Size = new System.Drawing.Size(512, 23);
+            this.txbLink.TabIndex = 2;
+            // 
+            // btnLink
+            // 
+            this.btnLink.Location = new System.Drawing.Point(573, 53);
+            this.btnLink.Name = "btnLink";
+            this.btnLink.Size = new System.Drawing.Size(75, 24);
+            this.btnLink.TabIndex = 3;
+            this.btnLink.Text = "Exibir";
+            this.btnLink.UseVisualStyleBackColor = true;
+            this.btnLink.Click += new System.EventHandler(this.btnLink_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(14, 98);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Vídeo 1:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(14, 125);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Vídeo 2:";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkLabel1.Location = new System.Drawing.Point(68, 94);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(316, 17);
+            this.linkLabel1.TabIndex = 6;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "https://www.youtube.com/watch?v=MTTP5j4a77E";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // linkLabel2
+            // 
+            this.linkLabel2.AutoSize = true;
+            this.linkLabel2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkLabel2.Location = new System.Drawing.Point(68, 123);
+            this.linkLabel2.Name = "linkLabel2";
+            this.linkLabel2.Size = new System.Drawing.Size(305, 17);
+            this.linkLabel2.TabIndex = 7;
+            this.linkLabel2.TabStop = true;
+            this.linkLabel2.Text = "https://www.youtube.com/watch?v=8eNxZI-3Bxs";
+            this.linkLabel2.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel2_LinkClicked);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(17, 156);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(630, 420);
+            this.webBrowser1.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(660, 586);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.linkLabel2);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnLink);
+            this.Controls.Add(this.txbLink);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Vídeo do Youtube";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txbLink;
+        private System.Windows.Forms.Button btnLink;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.LinkLabel linkLabel2;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+    }
+}
+
---
+++ 11-WebBrowser/WebBrowser/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WebBrowser
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            try
+            {
+                string html = "<html><head>";
+                html += "<meta content='IE=Edge' http-equiv='X-UA-Compatible'>";
+                html += "</head><body><iframe id='video' src='https://www.youtube.com/embed/{0}' width='600' height='400' frameborder='0'></iframe>";
+                html += "</body></html>";
+
+                this.webBrowser1.DocumentText = string.Format(html, linkLabel1.Text.Split('=')[1]);
+            }
+            catch (Exception)
+            {
+
+                
+            }
+        }
+
+        private void linkLabel2_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            try
+            {
+                string html = "<html><head>";
+                html += "<meta content='IE=Edge' http-equiv='X-UA-Compatible'>";
+                html += "</head><body><iframe id='video' src='https://www.youtube.com/embed/{0}' width='600' height='400' frameborder='0'></iframe>";
+                html += "</body></html>";
+
+                this.webBrowser1.DocumentText = string.Format(html, linkLabel2.Text.Split('=')[1]);
+            }
+            catch (Exception)
+            {
+
+
+            }
+        }
+
+        private void btnLink_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string html = "<html><head>";
+                html += "<meta content='IE=Edge' http-equiv='X-UA-Compatible'>";
+                html += "</head><body><iframe id='video' src='https://www.youtube.com/embed/{0}' width='600' height='400' frameborder='0'></iframe>";
+                html += "</body></html>";
+
+                this.webBrowser1.DocumentText = string.Format(html, txbLink.Text.Split('=')[1]);
+            }
+            catch (Exception)
+            {
+
+
+            }
+        }
+    }
+}
