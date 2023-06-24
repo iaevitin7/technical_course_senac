f0099f4a704b830cc92c8d0c10557e827e9908c2
João Victor G. de Carvalho <136175136+iaevitin7@users.noreply.github.com>
2023-06-24 15:10:23 -0300

    Upload BMI calculation - Project

---
+++ 08-IMC/IMC/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace IMC
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txbPeso = new System.Windows.Forms.TextBox();
+            this.txbAltura = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.lblTexto = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(265, 39);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Cálculo de IMC";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(280, 10);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Digite seu peso:";
+            // 
+            // txbPeso
+            // 
+            this.txbPeso.Location = new System.Drawing.Point(283, 28);
+            this.txbPeso.Name = "txbPeso";
+            this.txbPeso.Size = new System.Drawing.Size(100, 20);
+            this.txbPeso.TabIndex = 3;
+            this.txbPeso.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txbPeso_KeyPress);
+            // 
+            // txbAltura
+            // 
+            this.txbAltura.Location = new System.Drawing.Point(411, 28);
+            this.txbAltura.Name = "txbAltura";
+            this.txbAltura.Size = new System.Drawing.Size(100, 20);
+            this.txbAltura.TabIndex = 5;
+            this.txbAltura.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txbAltura_KeyPress);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(408, 10);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Digite sua altura:";
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(354, 274);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 35);
+            this.btnCalcular.TabIndex = 6;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultado.Location = new System.Drawing.Point(19, 264);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(96, 20);
+            this.lblResultado.TabIndex = 7;
+            this.lblResultado.Text = "Resultado:";
+            // 
+            // lblTexto
+            // 
+            this.lblTexto.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTexto.Location = new System.Drawing.Point(19, 287);
+            this.lblTexto.Name = "lblTexto";
+            this.lblTexto.Size = new System.Drawing.Size(201, 26);
+            this.lblTexto.TabIndex = 8;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::IMC.Properties.Resources.tabela_IMC;
+            this.pictureBox2.Location = new System.Drawing.Point(23, 330);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(488, 437);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 9;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::IMC.Properties.Resources.imc;
+            this.pictureBox1.Location = new System.Drawing.Point(19, 63);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(492, 194);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(436, 274);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(75, 35);
+            this.btnLimpar.TabIndex = 10;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(530, 321);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.lblTexto);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txbAltura);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txbPeso);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Cálculo de IMC";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txbPeso;
+        private System.Windows.Forms.TextBox txbAltura;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.Label lblTexto;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button btnLimpar;
+    }
+}
+
---
+++ 08-IMC/IMC/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace IMC
+{
+    public partial class Form1 : Form
+    {
+        double peso = 0;
+        double altura = 0;
+        double resultado = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            peso = Convert.ToDouble(txbPeso.Text);
+            altura = Convert.ToDouble(txbAltura.Text);
+            resultado = peso / (altura * altura);
+            lblResultado.Text = "Resultado: " + resultado.ToString("N2");
+
+            if (resultado < 16.9)
+            {
+                lblTexto.ForeColor = Color.Red;
+                lblTexto.Text = "Muito abaixo do peso";
+            }
+            else if (resultado >= 17 && resultado <= 18.4)
+            {
+                lblTexto.ForeColor = Color.FromArgb(255, 128, 0); 
+                lblTexto.Text = "Abaixo do peso";
+            }
+            else if (resultado >= 18.5 && resultado <= 24.9)
+            {
+                lblTexto.ForeColor = Color.Blue;
+                lblTexto.Text = "Peso normal";
+            }
+            else if (resultado >= 25 && resultado <= 29.9)
+            {
+                lblTexto.ForeColor = Color.FromArgb(255, 128, 0); 
+                lblTexto.Text = "Acima do peso";
+            }
+            else if (resultado >= 30 && resultado <= 34.9)
+            {
+                lblTexto.ForeColor = Color.Red;
+                lblTexto.Text = "Obesidade 1";
+            }
+            else if (resultado >= 35 && resultado <= 40)
+            {
+                lblTexto.ForeColor = Color.Red;
+                lblTexto.Text = "Obesidade 2";
+            }
+            else
+            {
+                lblTexto.ForeColor = Color.Red;
+                lblTexto.Text = "Obesidade 3";
+            }
+
+            // Definindo o tamanho da tela
+            this.Size = new Size(546, 820);
+            // Encontrando o centro do comprimento
+            int newX = (Screen.PrimaryScreen.Bounds.Width - this.Width) / 2;
+            // Encontrado o centro da largura
+            int newY = (Screen.PrimaryScreen.Bounds.Height - this.Height) / 2;
+            // Reposicionando o form no centro da tela
+            this.Location = new Point(newX, newY);
+        }
+
+        private void txbAltura_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            /*
+            // Verifica se foi digitado um ponto e subtitui por vírgula
+            if (e.KeyChar == '.')
+            {
+                e.KeyChar = ',';
+            }*/
+
+
+            // Verifica se o caractere digitado não é um número, uma vírgula ou a tecla Backspace
+            if (!char.IsDigit(e.KeyChar) && e.KeyChar != ',' && e.KeyChar != 8)
+            {
+                // Cancela o evento KeyPress, impedindo que o caractere seja inserido no TextBox
+                e.Handled = true;
+            }
+
+            // Verifica se o caractere digitado é uma vírgula
+            if (e.KeyChar == ',')
+            {
+                // Verifica se já existe uma vírgula no TextBox
+                if (txbAltura.Text.Contains(","))
+                {
+                    // Cancela o evento KeyPress, impedindo que a segunda vírgula seja inserida
+                    e.Handled = true;
+                }
+            }
+        }
+
+        private void txbPeso_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            /*
+            // Verifica se foi digitado um ponto e subtitui por vírgula
+            if (e.KeyChar == '.')
+            {
+                e.KeyChar = ',';
+            }*/
+
+
+            // Verifica se o caractere digitado não é um número, uma vírgula ou a tecla Backspace
+            if (!char.IsDigit(e.KeyChar) && e.KeyChar != ',' && e.KeyChar != 8)
+            {
+                // Cancela o evento KeyPress, impedindo que o caractere seja inserido no TextBox
+                e.Handled = true;
+            }
+
+            // Verifica se o caractere digitado é uma vírgula
+            if (e.KeyChar == ',')
+            {
+                // Verifica se já existe uma vírgula no TextBox
+                if (txbPeso.Text.Contains(","))
+                {
+                    // Cancela o evento KeyPress, impedindo que a segunda vírgula seja inserida
+                    e.Handled = true;
+                }
+            }
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            txbAltura.Clear();
+            txbPeso.Clear();
+            altura = 0;
+            peso = 0;
+            resultado = 0;
+
+            lblResultado.Text = "Resultado: ";
+            lblTexto.Text = string.Empty;
+
+            // Definindo o tamanho da tela
+            this.Size = new Size(546, 360);
+            // Encontrando o centro do comprimento
+            int newX = (Screen.PrimaryScreen.Bounds.Width - this.Width) / 2;
+            // Encontrado o centro da largura
+            int newY = (Screen.PrimaryScreen.Bounds.Height - this.Height) / 2;
+            // Reposicionando o form no centro da tela
+            this.Location = new Point(newX, newY);
+            txbPeso.Focus();
+        }
+    }
+}
