61cec994f8a0363e972730e525059cd752374ef2
João Victor G. de Carvalho <136175136+iaevitin7@users.noreply.github.com>
2023-06-24 14:16:32 -0300

    Upload basic components project

---
+++ 01-ComponentesBasicos/ComponentesBasicos/frmPrincipal.Designer.cs
@@ -0,0 +1,368 @@
+﻿namespace ComponentesBasicos
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblCheck = new System.Windows.Forms.Label();
+            this.lblMask = new System.Windows.Forms.Label();
+            this.lblData = new System.Windows.Forms.Label();
+            this.btnMostrar = new System.Windows.Forms.Button();
+            this.lblRadioButton = new System.Windows.Forms.Label();
+            this.txbTexto = new System.Windows.Forms.TextBox();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.lblListView = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.lblListBox = new System.Windows.Forms.Label();
+            this.lblComboBox = new System.Windows.Forms.Label();
+            this.lblTexto = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::ComponentesBasicos.Properties.Resources.smurf;
+            this.pictureBox1.Location = new System.Drawing.Point(142, 294);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 93);
+            this.pictureBox1.TabIndex = 54;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(267, 294);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(86, 13);
+            this.label7.TabIndex = 53;
+            this.label7.Text = "Digite seu nome:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 234);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(95, 13);
+            this.label6.TabIndex = 52;
+            this.label6.Text = "Digite um telefone:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(293, 69);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(93, 13);
+            this.label5.TabIndex = 51;
+            this.label5.Text = "Selecione um item";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(149, 69);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(93, 13);
+            this.label4.TabIndex = 50;
+            this.label4.Text = "Selecione um item";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(143, 233);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(104, 13);
+            this.label3.TabIndex = 49;
+            this.label3.Text = "Selecione uma data:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 13);
+            this.label2.TabIndex = 48;
+            this.label2.Text = "Selecione um item:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(28, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(358, 39);
+            this.label1.TabIndex = 47;
+            this.label1.Text = "Componentes Básicos";
+            // 
+            // lblCheck
+            // 
+            this.lblCheck.AutoSize = true;
+            this.lblCheck.Location = new System.Drawing.Point(16, 331);
+            this.lblCheck.Name = "lblCheck";
+            this.lblCheck.Size = new System.Drawing.Size(0, 13);
+            this.lblCheck.TabIndex = 46;
+            // 
+            // lblMask
+            // 
+            this.lblMask.AutoSize = true;
+            this.lblMask.Location = new System.Drawing.Point(12, 273);
+            this.lblMask.Name = "lblMask";
+            this.lblMask.Size = new System.Drawing.Size(0, 13);
+            this.lblMask.TabIndex = 45;
+            // 
+            // lblData
+            // 
+            this.lblData.AutoSize = true;
+            this.lblData.Location = new System.Drawing.Point(143, 273);
+            this.lblData.Name = "lblData";
+            this.lblData.Size = new System.Drawing.Size(0, 13);
+            this.lblData.TabIndex = 44;
+            // 
+            // btnMostrar
+            // 
+            this.btnMostrar.Location = new System.Drawing.Point(15, 355);
+            this.btnMostrar.Name = "btnMostrar";
+            this.btnMostrar.Size = new System.Drawing.Size(75, 32);
+            this.btnMostrar.TabIndex = 43;
+            this.btnMostrar.Text = "Mostrar";
+            this.btnMostrar.UseVisualStyleBackColor = true;
+            this.btnMostrar.Click += new System.EventHandler(this.btnMostrar_Click);
+            // 
+            // lblRadioButton
+            // 
+            this.lblRadioButton.AutoSize = true;
+            this.lblRadioButton.Location = new System.Drawing.Point(12, 187);
+            this.lblRadioButton.Name = "lblRadioButton";
+            this.lblRadioButton.Size = new System.Drawing.Size(0, 13);
+            this.lblRadioButton.TabIndex = 42;
+            // 
+            // txbTexto
+            // 
+            this.txbTexto.Location = new System.Drawing.Point(270, 311);
+            this.txbTexto.Name = "txbTexto";
+            this.txbTexto.Size = new System.Drawing.Size(100, 20);
+            this.txbTexto.TabIndex = 41;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(15, 156);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(67, 17);
+            this.radioButton2.TabIndex = 40;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Opção B";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(15, 135);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(67, 17);
+            this.radioButton1.TabIndex = 39;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Opção A";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(15, 250);
+            this.maskedTextBox1.Mask = "(00) 00000-0000";
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(100, 20);
+            this.maskedTextBox1.TabIndex = 38;
+            // 
+            // lblListView
+            // 
+            this.lblListView.AutoSize = true;
+            this.lblListView.Location = new System.Drawing.Point(293, 185);
+            this.lblListView.Name = "lblListView";
+            this.lblListView.Size = new System.Drawing.Size(0, 13);
+            this.lblListView.TabIndex = 37;
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(296, 85);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(120, 97);
+            this.listView1.TabIndex = 36;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(152, 87);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 95);
+            this.listBox1.TabIndex = 35;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // lblListBox
+            // 
+            this.lblListBox.AutoSize = true;
+            this.lblListBox.Location = new System.Drawing.Point(149, 185);
+            this.lblListBox.Name = "lblListBox";
+            this.lblListBox.Size = new System.Drawing.Size(0, 13);
+            this.lblListBox.TabIndex = 34;
+            // 
+            // lblComboBox
+            // 
+            this.lblComboBox.AutoSize = true;
+            this.lblComboBox.Location = new System.Drawing.Point(12, 109);
+            this.lblComboBox.Name = "lblComboBox";
+            this.lblComboBox.Size = new System.Drawing.Size(0, 13);
+            this.lblComboBox.TabIndex = 33;
+            // 
+            // lblTexto
+            // 
+            this.lblTexto.AutoSize = true;
+            this.lblTexto.Location = new System.Drawing.Point(267, 334);
+            this.lblTexto.Name = "lblTexto";
+            this.lblTexto.Size = new System.Drawing.Size(0, 13);
+            this.lblTexto.TabIndex = 32;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(146, 250);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(225, 20);
+            this.dateTimePicker1.TabIndex = 31;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Item 1",
+            "Item 2",
+            "Item 3"});
+            this.comboBox1.Location = new System.Drawing.Point(15, 85);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 30;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(15, 311);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(58, 17);
+            this.checkBox1.TabIndex = 29;
+            this.checkBox1.Text = "Opção";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(430, 401);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblCheck);
+            this.Controls.Add(this.lblMask);
+            this.Controls.Add(this.lblData);
+            this.Controls.Add(this.btnMostrar);
+            this.Controls.Add(this.lblRadioButton);
+            this.Controls.Add(this.txbTexto);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Controls.Add(this.lblListView);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.lblListBox);
+            this.Controls.Add(this.lblComboBox);
+            this.Controls.Add(this.lblTexto);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.checkBox1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmPrincipal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Componentes Básicos";
+            this.Load += new System.EventHandler(this.frmPrincipal_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblCheck;
+        private System.Windows.Forms.Label lblMask;
+        private System.Windows.Forms.Label lblData;
+        private System.Windows.Forms.Button btnMostrar;
+        private System.Windows.Forms.Label lblRadioButton;
+        private System.Windows.Forms.TextBox txbTexto;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.Label lblListView;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label lblListBox;
+        private System.Windows.Forms.Label lblComboBox;
+        private System.Windows.Forms.Label lblTexto;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ 01-ComponentesBasicos/ComponentesBasicos/frmPrincipal.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ComponentesBasicos
+{
+    public partial class frmPrincipal : Form
+    {
+        public frmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void frmPrincipal_Load(object sender, EventArgs e)
+        {
+            // Popula a ComboBox
+            comboBox1.Items.AddRange(new string[] { "Item 4", "Item 5", "Item 6" });
+
+            // Popula a ListBox
+            listBox1.Items.AddRange(new string[] { "Item 1", "Item 2", "Item 3" });
+
+            // Configura o listView
+            listView1.View = View.Details;
+            listView1.AutoResizeColumns(ColumnHeaderAutoResizeStyle.None);
+
+            // Adiciona colunas à ListView
+            listView1.Columns.Add("Nome", 50);
+            listView1.Columns.Add("Idade", 50);
+
+            // Popula a ListView
+            listView1.Items.Add(new ListViewItem(new string[] { "João", "30" }));
+            listView1.Items.Add(new ListViewItem(new string[] { "Maria", "25" }));
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string item = comboBox1.SelectedItem.ToString();
+            lblComboBox.Text = "Selecionou: " + item;
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string list = listBox1.SelectedItem.ToString();
+            lblListBox.Text = "Selecionou: " + list;
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count > 0)
+            {
+                ListViewItem selectedItem = listView1.SelectedItems[0];
+                string nome = selectedItem.SubItems[0].Text;
+                string idade = selectedItem.SubItems[1].Text;
+                lblListView.Text = "Selecionou: " + nome + ", " + idade;
+            }
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                lblRadioButton.Text = "Opção A Selecionada";
+            }
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton2.Checked)
+            {
+                lblRadioButton.Text = "Opção B Selecionada";
+            }
+        }
+
+        private void btnMostrar_Click(object sender, EventArgs e)
+        {
+            string telefone = maskedTextBox1.Text;
+            lblMask.Text = "Fone: " + telefone;
+
+            string data = dateTimePicker1.Value.ToString("dd/MM/yyyy");
+            lblData.Text = "Data selecionada: " + data;
+
+            if (checkBox1.Checked)
+            {
+                lblCheck.Text = "Opção selecionada";
+            }
+            else
+            {
+                lblCheck.Text = "Opção não selecionada";
+            }
+
+            string texto = txbTexto.Text;
+            lblTexto.Text = "Olá " + texto + "\nBem vindo ao Windows Forms";
+
+            pictureBox1.Image = Properties.Resources.smurf;
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+        }
+    }
+}
