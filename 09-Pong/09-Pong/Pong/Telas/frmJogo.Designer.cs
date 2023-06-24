b7c60a15e23c6638a14d682a77621e882fad6a42
João Victor G. de Carvalho <136175136+iaevitin7@users.noreply.github.com>
2023-06-24 14:13:34 -0300

    Upload course projects

---
+++ 09-Pong/09-Pong/Pong/Telas/frmJogo.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace Pong.Telas
+{
+    partial class frmJogo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmJogo));
+            this.picBolinha = new System.Windows.Forms.PictureBox();
+            this.picRaquete = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.picBolinha)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picRaquete)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picBolinha
+            // 
+            this.picBolinha.Image = global::Pong.Properties.Resources.bola;
+            this.picBolinha.Location = new System.Drawing.Point(12, 12);
+            this.picBolinha.Name = "picBolinha";
+            this.picBolinha.Size = new System.Drawing.Size(30, 30);
+            this.picBolinha.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.picBolinha.TabIndex = 0;
+            this.picBolinha.TabStop = false;
+            // 
+            // picRaquete
+            // 
+            this.picRaquete.BackColor = System.Drawing.Color.Black;
+            this.picRaquete.Location = new System.Drawing.Point(768, 140);
+            this.picRaquete.Name = "picRaquete";
+            this.picRaquete.Size = new System.Drawing.Size(13, 170);
+            this.picRaquete.TabIndex = 1;
+            this.picRaquete.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 20;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // frmJogo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 461);
+            this.Controls.Add(this.picRaquete);
+            this.Controls.Add(this.picBolinha);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmJogo";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Pong";
+            this.Load += new System.EventHandler(this.frmJogo_Load);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.frmJogo_MouseMove);
+            ((System.ComponentModel.ISupportInitialize)(this.picBolinha)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picRaquete)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picBolinha;
+        private System.Windows.Forms.PictureBox picRaquete;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
---
+++ 09-Pong/09-Pong/Pong/Telas/frmJogo.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pong.Telas
+{
+    public partial class frmJogo : Form
+    {
+
+        // Variáveis
+        int velocidade, resultado;
+        bool topo, esquerda;
+
+        private void frmJogo_Load(object sender, EventArgs e)
+        {
+            Random bola = new Random();
+            picBolinha.Location = new Point(0, bola.Next(this.Height));
+            topo = esquerda = true;
+            timer1.Enabled = true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // Verifica se a bolinha passou pela raquete
+            if (picBolinha.Left > picRaquete.Left)
+            {
+                timer1.Enabled = false;
+                if (resultado == 1)
+                {
+                    MessageBox.Show("Perdeu!!! \nVocê fez " + resultado.ToString() + " ponto", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("Perdeu!!! \nVocê fez " + resultado.ToString() + " pontos", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                resultado = 0;
+            }
+
+            if (picBolinha.Left + picBolinha.Width >= picRaquete.Left && 
+                picBolinha.Left + picBolinha.Width <= picRaquete.Left + picRaquete.Width &&
+                picBolinha.Top + picBolinha.Height >= picRaquete.Top &&
+                picBolinha.Top + picBolinha.Height <= picRaquete.Top + picRaquete.Height + picBolinha.Height)
+            {
+                esquerda = false;
+                resultado += 1;
+                SoundPlayer Som = new SoundPlayer(@"pong.wav");
+                Som.Play();
+                this.Text = "Seus pontos: " + resultado.ToString();
+            }
+
+            if (esquerda)
+                picBolinha.Left += velocidade;
+            else
+                picBolinha.Left -= velocidade;
+            if(topo)
+                picBolinha.Top += velocidade;
+            else
+                picBolinha.Top -= velocidade;
+            if(picBolinha.Top >= this.Height - 70)
+                topo = false;
+            if (picBolinha.Top <= 5)
+                topo = true;
+            if (picBolinha.Left <= 0)
+                esquerda = true;
+        }
+
+        public frmJogo(int valor, int velo)
+        {
+            InitializeComponent();
+            if (valor == 0 )
+            {
+                valor = 100;
+            }
+            else
+            {
+                picRaquete.Height = valor;
+            }
+            velocidade = velo;
+        }
+
+        private void frmJogo_MouseMove(object sender, MouseEventArgs e)
+        {
+            picRaquete.Top = e.Y - (picRaquete.Height / 2 );
+        }
+    }
+}
